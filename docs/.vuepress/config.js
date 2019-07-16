18ea18a27b8a52901145a159b4c2a14fef005e7e
Juan Carlos Aguilera <jcaguile@uc.cl>
2019-07-16 02:19:47 -0400

    Added drafts for all 10 scenarios

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	title: "Hello World",
-	description: "Carlos' Outbreak minisite"
+	title: "Carlos' Outbreak Minisite",
+	description: "Guides for getting through scenarios in Resident Evil: Outbreak and Resident Evil: Outbreak - File #2",
 }
