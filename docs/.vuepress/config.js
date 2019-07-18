5d20580207be0c82a8638ee50bbab3b284017fe4
Juan Carlos Aguilera <jcaguile@uc.cl>
2019-07-17 20:06:27 -0400

    Added sidebar and two placeholder pages

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,31 @@
 module.exports = {
 	title: "Carlos' Outbreak Minisite",
 	description: "Guides for getting through scenarios in Resident Evil: Outbreak and Resident Evil: Outbreak - File #2",
+	themeConfig: {
+		sidebar: {
+			'/characters/' : [
+				'kevin',
+				'mark',
+				'jim',
+				'george',
+				'david',
+				'alyssa',
+				'yoko',
+				'cindy'
+			],
+			'/scenarios/': [
+				'outbreak',
+				'below-freezing-point',
+				'the-hive',
+				'hellfire',
+				'decisions-decisions',
+				'wild-things',
+				'underbelly',
+				'flashback',
+				'desperate-times',
+				'end-of-the-road'
+				]
+			
+		}
+	}
 }
