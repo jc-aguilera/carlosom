a46b01afcac6b9e80484406c30e92aa7be58e768
Juan Carlos Aguilera <jcaguile@uc.cl>
2019-11-03 00:22:11 -0300

    Added favicons

--- docs/.vuepress/components/ScenarioOverviewCard.vue
+++ docs/.vuepress/components/ScenarioOverviewCard.vue
@@ -1,20 +1,29 @@
 <template>
-<div>
+  <div class="flex-column">
     <div>
-        <img :src="$frontmatter.scenarioPic">
+      <img :src="$frontmatter.scenarioPic" />
     </div>
     <div>
-        <div>
-            <ul>
-                <li v-for="o in $frontmatter.objectives">{{ o }}</li>
-            </ul>
-        </div>
+      <div>
+        <ul>
+          <li v-for="o in $frontmatter.objectives">{{ o }}</li>
+        </ul>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'ScenarioOverviewCard'
-}
+  name: "ScenarioOverviewCard"
+};
 </script>
+
+<style lang="stylus">
+.flex-column {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
